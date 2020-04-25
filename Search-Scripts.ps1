7400b23768b1f17fd74f2ba5e1ec32456b65e582
Roy Coutts <roy.coutts@gmail.com>
2020-04-25 00:04:26 -0400

    First Commit

---
+++ Search-Scripts.ps1
@@ -0,0 +1,25 @@
+﻿FUNCTION Search-Scripts {
+    PARAM(
+        [STRING[]]$Path = $pwd,
+        [STRING[]]$Include = "*.ps1",
+        [STRING[]]$KeyWord = (Read-Host "Keyword?"),
+        [SWITCH]$ListView
+    )
+    BEGIN {
+
+    }
+    PROCESS {
+        Get-ChildItem -path $Path -Include $Include -Recurse | `
+	    sort Directory,CreationTime | `
+	    Select-String -simplematch $KeyWord -OutVariable Result | `
+        Out-Null
+    }
+    END {
+        IF ($ListView) { 
+            $Result | Format-List -Property Path,LineNumber,Line 
+        } 
+        ELSE { 
+            $Result | Format-Table -GroupBy Path -Property LineNumber,Line -AutoSize 
+        } 
+    }
+}
